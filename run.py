4505ab94bdb6f3aa902f372a26fc1150e9a00440
arcadejk Kakpo <arcadej@hotmail.fr>
2024-05-23 18:42:03 +0000

    Create choose_to_continue function and variable

--- run.py
+++ run.py
@@ -25,3 +25,22 @@ print(f"Hello, {user_name}! Welcome to this challenge.")
 print("You are about to start a game of 3 steps.")
 print("Soon you will start the Hangman game to guess the right word.")
 
+def choose_to_continue():
+    """
+    Create loop to ask and return user choice,
+    if he wants or not to start the game.
+    """
+    while True:
+        choice = input("Would you like to enter your username? (y/n): ").lower()
+        if choice == 'y':
+            print(f"Let's start, {user_name}!")
+            return get_word
+        elif choice == 'n':
+            print("No problem, you can come back anytime.")
+            return None
+        else:
+            print("Invalid choice. Please enter 'y' for yes or 'n' for no.")
+
+choose_to_continue = choose_to_continue()
+print(choose_to_continue)
+
